63ff54dc0fe9a453b93f5d78b9fc5038935fbc72
Ujaan Das <ujaandas03@gmail.com>
2025-02-20 00:25:16 +0800

    init jekyll

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Example development environment flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        packages = with pkgs; [
+          bundler
+          jekyll
+        ];
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = packages;
+          shellHook = ''
+            echo "Welcome to the development shell!"
+            zsh
+          '';
+        };
+      });
+}
